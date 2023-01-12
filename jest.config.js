d243e0a84d53e15c4cddbbf774097f31370b1e03
Paul de Koning <pauldekoning01@gmail.com>
2023-01-12 19:35:04 +0100

    Add readme.md

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  "rootDir": "./",
-  "preset": "ts-jest",
-  "testEnvironment": "node",
-  "transformIgnorePatterns": ['node_modules'],
-  "collectCoverageFrom": [
-    "src/**/*.ts*"
-  ]
+    "rootDir": "./",
+    "preset": "ts-jest",
+    "testEnvironment": "node",
+    "transformIgnorePatterns": ['node_modules'],
+    "coverageReporters": [
+      "json-summary",
+      "text"
+    ]
 };
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Daft Parser
+
+![Test coverage statements](coverage/badge-statements.svg) 
+![Test coverage branches](coverage/badge-branches.svg) 
+![Test coverage functions](coverage/badge-functions.svg) 
+![Test coverage lines](coverage/badge-lines.svg)
+
+--------------
+This project was created to parse listings from the popular Irish property website https://daft.ie/
